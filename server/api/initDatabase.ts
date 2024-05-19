3ccb47c5857301c397654528827755e674ec8a90
Sam Nguyen <sam.nguyen.1223@gmail.com>
2024-05-19 08:38:06 -0400

    Add basic database structure

---
+++ server/api/initDatabase.ts
@@ -0,0 +1,51 @@
+import { createRequire } from 'module'
+
+export default defineEventHandler((event) => {
+    const require = createRequire(import.meta.url)
+    const sqlite3 = require('sqlite3').verbose()
+    const db = new sqlite3.Database('./data/data.db')
+
+    db.serialize(() => {
+        db.run(`
+            CREATE TABLE IF NOT EXISTS institutions (
+                id INTEGER PRIMARY KEY,
+                name TEXT NOT NULL,
+                url TEXT,
+                icon TEXT
+            )
+        `)
+
+        db.run(`
+            CREATE TABLE IF NOT EXISTS accounts (
+                id INTEGER PRIMARY KEY,
+                name TEXT,
+                account_number INTEGER,
+                institution_id INTEGER NOT NULL,
+                initial_balance INTEGER NOT NULL,
+                current_balance INTEGER NOT NULL,
+
+                FOREIGN KEY (institution_id)
+                    REFERENCES institutions (id)
+            )
+        `)
+
+        db.run(`
+            CREATE TABLE IF NOT EXISTS transactions (
+                id INTEGER PRIMARY KEY,
+                type TEXT NOT NULL,
+                amount INTEGER NOT NULL,
+                primary_account INTEGER NOT NULL,
+                secondary_account INTEGER,
+
+                FOREIGN KEY (primary_account)
+                    REFERENCES accounts (id),
+
+                FOREIGN KEY (secondary_account)
+                    REFERENCES accounts (id)
+            )
+        `)
+    })
+
+    db.close()
+    return('maybe success')
+})
