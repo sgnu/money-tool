7461b4011e40bb8ff2631699a814ab84b5e5b7a7
Sam Nguyen <sam.nguyen.1223@gmail.com>
2024-05-28 16:09:06 -0400

    Add data type for accounts

--- types/index.ts
+++ types/index.ts
@@ -7,4 +7,23 @@ declare global {
         url: string,
         icon: string
     }
+
+    interface Account {
+        id: number,
+        name: string,
+        accountNumber: number,
+        accountType: AccountTypes,
+        institutionId: number,
+        initialBalance: number,
+        currentBalance: number,
+    }
+
+    enum AccountTypes {
+        checkings,
+        savings,
+        credit,
+        brokerage,
+        asset,
+        liability
+    }
 }
