437df4c41408c5be3dbcca4f2622267d9e277897
Sam Nguyen <sam.nguyen.1223@gmail.com>
2024-06-01 12:42:35 -0400

    Create basic CRUD for accounts

---
+++ server/api/accounts/create.post.ts
@@ -0,0 +1,20 @@
+import { createRequire } from 'module'
+
+export default defineEventHandler(async (event) => {
+    const require = createRequire(import.meta.url)
+    const sqlite3 = require('sqlite3').verbose
+    const db = new sqlite3.Database('./data/data.db')
+
+    const body:Account = await readBody(event)
+
+    const data = db.run(`
+        INSERT INTO accounts (name, account_number, account_type, institution_id, initial_balance, current_balance)
+        VALUES (?, ?, ?, ?, ?, ?)
+        RETURNING *
+    `, [body.name, body.accountNumber, body.accountType, body.institutionId, body.initialBalance, body.currentBalance])
+    // when submitting initial data, current balance should be set equal to initial balance
+
+    db.close()
+
+    return data
+})
---
+++ server/api/accounts/update.put.ts
@@ -0,0 +1,20 @@
+import { createRequire } from 'module'
+
+export default defineEventHandler(async (event) => {
+    const require = createRequire(import.meta.url)
+    const sqlite3 = require('sqlite3').verbose()
+    const db = new sqlite3.Database('./data/data.db')
+
+    const body:Account = await readBody(event)
+
+    const data = db.run(`
+        UPDATE institutions
+        SET name=?, account_number=?, account_type=?, institution_id=?, initial_balance=?, current_balance=?
+        WHERE id=?
+        RETURNING *
+    `, [body.name, body.accountNumber, body.accountType, body.institutionId, body.initialBalance, body.currentBalance, body.id])
+
+    db.close()
+
+    return data
+})
