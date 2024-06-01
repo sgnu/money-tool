19603c23cc69b0b824cd6c2ba5562f95fc0c9ca4
Sam Nguyen <sam.nguyen.1223@gmail.com>
2024-06-01 12:42:51 -0400

    Add basic page transition

--- app.vue
+++ app.vue
@@ -5,3 +5,16 @@
     </div>
   </div>
 </template>
+
+<style>
+.page-enter-active,
+.page-leave-active {
+  transition: all 0.1s ease-out;
+}
+
+.page-enter-from,
+.page-leave-to {
+  opacity: 0;
+  transform: translateY(24px);
+}
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  app: {
+    pageTransition: { name: 'page', mode: 'out-in' },
+    head: {
+      meta: [
+        { name: 'viewport', content: 'width=device-width, initial-scale=1, minimum-scale=1, user-scalable=no viewport-fit=cover' }
+      ]
+    }
+  },
   modules: ["@nuxtjs/tailwindcss"],
   postcss: {
     plugins: {
