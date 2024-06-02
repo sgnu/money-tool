a62b57c91bcdbd59aa07a29da64e3769b41527f3
Sam Nguyen <sam.nguyen.1223@gmail.com>
2024-06-02 12:33:27 -0400

    Differentiate between asset and liability in display

--- components/AccountCard.vue
+++ components/AccountCard.vue
@@ -1,21 +1,28 @@
 <script setup lang="ts">
+import { AccountTypes } from '~/types/AccountTypes';
+
 const props = defineProps<{
     account: Account,
     institution: Institution
   }>()
+
+  const isLiability = computed(() => {
+    return props.account.accountType === AccountTypes.CREDIT || props.account.accountType === AccountTypes.LIABILITY
+  })
 
 </script>
 
 <template>
   <div class="card w-64 bg-base-100 shadow-xl">
     <div class="card-body">
-      <NuxtLink class="card-title" to="TODO: goto account info">
+      <NuxtLink class="card-title" to="/">
         <h2>
-          <img :src="institution.icon">
-          {{ account.name }} ...{{ account.accountNumber }}
+          <img :src="institution.icon" class="w-8 h-8 inline">
+          {{ account.name }} {{ account.accountNumber }}
         </h2>
       </NuxtLink>
-      <p class="text-right">${{ account.currentBalance }}</p>
+      <p v-if="isLiability" class="text-right">$({{ account.currentBalance }})</p>
+      <p v-else class="text-right">${{ account.currentBalance }}</p>
     </div>
   </div>
 </template>
