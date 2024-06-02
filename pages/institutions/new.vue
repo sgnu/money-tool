04e758920cfbbd1411b5b91d9006b4c67ddee087
Sam Nguyen <sam.nguyen.1223@gmail.com>
2024-06-02 07:37:54 -0400

    Change loading modal to use boolean

--- pages/institutions/new.vue
+++ pages/institutions/new.vue
@@ -1,30 +1,28 @@
 <script setup lang="ts">
-let isLoading = true
+let isLoading = ref(false)
 const formData = reactive({
     name:  '',
     url: '',
     icon: ''
 })
 
-const submit = async () => {
-    loading_modal.showModal()
-    isLoading = true
-    await $fetch('/api/institutions/create', {
+const submit = () => {
+    isLoading.value = true
+    $fetch('/api/institutions/create', {
         method: 'POST',
         body: formData,
         onResponse() {
             setTimeout(() => {
-                // isLoading = false
+                isLoading.value = false
                 navigateTo('/institutions')
             }, 250)
         }
     })
-    console.log(formData)
 }
 </script>
 
 <template>
-    <div class="overflow-auto">
+    <div>
         <form class="max-w-96 flex flex-col gap-2 items-end shadow-md p-8 rounded">
             <label class="input input-bordered flex items-center gap-2 w-[100%]">
                 Name
@@ -40,10 +38,10 @@ const submit = async () => {
             </label>
             <button type="submit" class="btn btn-primary" @click.prevent="submit">Create</button>
         </form>
-        <dialog id="loading_modal" class="modal" v-if="isLoading">
+        <div role="dialog" :class="isLoading ? 'modal modal-open' : 'modal'">
             <div class="modal-box flex justify-center w-36">
                 <span class="loading loading-spinner loading-lg"></span>
             </div>
-        </dialog>
+        </div>
     </div>
 </template>
