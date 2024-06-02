84a6c01837f0c5d352816402c936226da0c93481
Sam Nguyen <sam.nguyen.1223@gmail.com>
2024-06-02 09:11:08 -0400

    Create components for daisyUI form components

---
+++ components/FormCard.vue
@@ -0,0 +1,5 @@
+<template>
+    <form class="w-96 flex flex-col gap-2 items-end shadow-md p-8 rounded">
+        <slot />
+    </form>
+</template>
---
+++ components/FormLabel.vue
@@ -0,0 +1,5 @@
+<template>
+    <label class="input input-bordered flex items-center gap-2 w-full">
+        <slot />
+    </label>
+</template>
