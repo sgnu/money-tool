3f33001e2ae49784302f420e80c7182b73f74b34
Sam Nguyen <sam.nguyen.1223@gmail.com>
2024-06-02 14:05:43 -0400

    Create skeleton card

---
+++ components/SkeletonCard.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="card w-64 bg-neutral shadow-xl">
+        <div class="card-body p-4">
+            <div class="flex gap-4 items-center">
+                <div class="skeleton w-12 h-12 rounded-full shrink-0"></div>
+                <div class="skeleton w-full h-8"></div>
+            </div>
+            <div class="skeleton w-full h-8"></div>
+        </div>
+    </div>
+</template>
