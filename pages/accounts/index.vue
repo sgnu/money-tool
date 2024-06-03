e3c5dc4093f0ecffcfbf541f72b8837e5650751a
Sam Nguyen <sam.nguyen.1223@gmail.com>
2024-06-03 02:38:24 -0400

    Fix loading when opening accounts page

--- pages/accounts/index.vue
+++ pages/accounts/index.vue
@@ -1,19 +1,27 @@
 <script setup lang="ts">
-const institutionMap = new Map()
-const { data: accounts } = await useFetch('/api/accounts/getAllAccounts')
-await useFetch('/api/institutions/getAllInstitutions', {
+const institutionMap = ref(new Map())
+let institutionLoading = ref(true)
+const { data: accounts, pending: accountLoading } = useFetch('/api/accounts/getAllAccounts', {
+})
+
+$fetch('/api/institutions/getAllInstitutions', {
     onResponse({ response }) {
-        (response._data as any).forEach((institution: Institution) => {
-            console.log(institution)
-            institutionMap.set(institution.id, institution)
+        (response._data as any[]).forEach((institution: Institution) => {
+            institutionMap.value.set(institution.id, institution)
         })
+        setTimeout(() => {
+            institutionLoading.value = false
+        }, 500)
     }
 })
 
 </script>
 
 <template>
-    <div class="grid grid-cols-1 lg:grid-cols-3 gap-4 place-items-center">
+    <div v-if="accountLoading || institutionLoading" class="grid grid-cols-1 lg:grid-cols-3 gap-4 place-items-center">
+        <SkeletonCard />
+    </div>
+    <div v-else class="grid grid-cols-1 lg:grid-cols-3 gap-4 place-items-center">
         <AccountCard  v-for="account in accounts" :account="account as Account" :institution="institutionMap.get(account.institutionId) as Institution" />
         <NuxtLink class="btn btn-outline btn-primary" to="/accounts/new">New</NuxtLink>
     </div>
