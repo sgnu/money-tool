aa0d5dffee5e5f49fd20bf177d1fb4e706b37f74
Sam Nguyen <sam.nguyen.1223@gmail.com>
2024-06-03 03:12:27 -0400

    Change base color to neutral

--- components/FormCard.vue
+++ components/FormCard.vue
@@ -1,5 +1,5 @@
 <template>
-    <form class="w-full lg:w-96 flex flex-col gap-2 items-end lg:shadow-md p-8 lg:rounded">
+    <form class="w-full lg:w-96 flex flex-col gap-2 items-end lg:shadow-md p-8 lg:rounded lg:bg-neutral">
         <slot />
     </form>
 </template>
