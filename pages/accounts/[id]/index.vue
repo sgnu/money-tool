14f713fb6c680832291b10e826d748fb7451dbb8
Sam Nguyen <sam.nguyen.1223@gmail.com>
2024-06-30 19:40:37 -0400

    Change new button to dropdown on narrower viewports

--- pages/accounts/[id]/index.vue
+++ pages/accounts/[id]/index.vue
@@ -56,6 +56,7 @@ const createTransaction = (type?: TransactionTypes) => {
         queries.type = type
     }
     
+    //TODO: finish handlings for types
     if (type === TransactionTypes.ADJUSTMENT) {
         queries.primaryAccount = account.id
     } else if (type === TransactionTypes.INCOME) {
@@ -85,19 +86,48 @@ const createTransaction = (type?: TransactionTypes) => {
         loading...
     </div>
     <div v-else>
-        <div class="flex items-baseline mb-2">
-            <h1 class="text-5xl basis-full">{{ account.name }} ...{{ account.accountNumber }}</h1>
-            <p class="text-2xl italic">
+        <div class="flex flex-wrap items-baseline mb-2">
+            <h1 class="text-5xl basis-auto">{{ account.name }} ...{{ account.accountNumber }}</h1>
+            <p class="justify-self-end text-2xl italic">
                 <span v-if="isLiability">$({{ balance }})</span>
                 <span v-else>${{ balance }}</span>
             </p>
+            <FlexBreak />
+            <p class="italic text-neutral-400">{{ account.accountType }}</p>
         </div>
         <div class="flex justify-end gap-x-2 gap-y-1 flex-wrap">
+            <div class="dropdown justify-self-start xl:hidden">
+                <div tabindex="0" class="btn btn-primary text-lg" role="button">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                        stroke="currentColor" class="size-6">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+                    </svg>
+                    New
+                </div>
+                <ul tabindex="0" class="menu dropdown-content shadow join join-vertical w-52 m-0">
+                    <button @click="createTransaction()" class="btn btn-sm btn-primary justify-start join-item">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                            stroke="currentColor" class="size-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+                        </svg>
+                        Transaction
+                    </button>
+                    <button v-for="transactionType in defaultTransactions" @click="createTransaction(transactionType)"
+                        class="btn btn-sm btn-primary join-item justify-start">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                            stroke="currentColor" class="size-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+                        </svg>
+                        {{ transactionType }}
+                    </button>
+                </ul>
+            </div>
             <NuxtLink class="btn btn-outline text-lg" :to="`/accounts/${route.params.id}/edit`">Edit</NuxtLink>
             <button class="btn btn-outline btn-error w-fit text-lg" @click="deleteAccount">Delete</button>
             <FlexBreak />
-            <div class="join">
-                <button @click="createTransaction()" class="btn btn-outline btn-sm btn-primary w-fit join-item">
+            <!-- TODO: change to drop down when small -->
+            <div class="join hidden xl:block xl:join-horizontal">
+                <button @click="createTransaction()" class="btn btn-outline btn-sm btn-primary join-item">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                         stroke="currentColor" class="size-6">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
@@ -105,7 +135,7 @@ const createTransaction = (type?: TransactionTypes) => {
                     Transaction
                 </button>
                 <button v-for="transactionType in defaultTransactions" @click="createTransaction(transactionType)"
-                    class="btn btn-outline btn-sm btn-primary w-fit join-item">
+                    class="btn btn-outline btn-sm btn-primary join-item justify-start">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                         stroke="currentColor" class="size-6">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
