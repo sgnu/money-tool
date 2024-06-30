d35f88af1d1cf778485804eeee52eec33e7b1649
Sam Nguyen <sam.nguyen.1223@gmail.com>
2024-06-30 17:05:17 -0400

    Add display for institution's accounts

--- pages/institutions/[id]/index.vue
+++ pages/institutions/[id]/index.vue
@@ -1,14 +1,23 @@
 <script setup lang="ts">
 const route = useRoute()
-const { data: institutionData } = await useFetch(`/api/institutions/${route.params.id}`)
-const institution:Institution = institutionData.value as Institution
+const { data: institutionData, pending: institutionLoading } = await useFetch(`/api/institutions/${route.params.id}`)
+const { data: accountData, pending: accountLoading } = await useFetch(`/api/institutions/${route.params.id}/accounts`)
+
+const institution: Institution = institutionData.value as Institution
+const accounts: Account[] = accountData.value as Account[]
 </script>
 
 <template>
-    <div>
+    <div v-if="accountLoading || institutionLoading">
+        <SkeletonCard />
+        <SkeletonCard />
+    </div>
+
+    <div v-else>
         <span><img :src="institution.icon" /><h1>{{ institution.name }}</h1></span>
-        <p></p>
-        <!-- <InstitutionCard :institution="institution"></InstitutionCard> -->
-        TODO: display accounts related to this institution
+        <!-- <p></p> -->
+        <div class="grid grid-cols-1 lg:grid-cols-3 gap-4 place-items-center">
+            <AccountCard v-for="account in accounts" :account="account" :institution="institution" />
+        </div>
     </div>
 </template>
