b6e289b7ff9cf5b48e272a3729b0ad8d287cf134
Sam Nguyen <sam.nguyen.1223@gmail.com>
2024-06-30 13:36:31 -0400

    Create global util for converting form sql to json

---
+++ utils/convertSQLTransaction.ts
@@ -0,0 +1,11 @@
+export default function(sqlTransaction: SQLTransaction | null): Transaction | null {
+    return sqlTransaction !== null ? {
+        name: sqlTransaction.name,
+        id: sqlTransaction.id,
+        type: sqlTransaction.type,
+        date: new Date(sqlTransaction.date),
+        amount: sqlTransaction.amount,
+        primaryAccount: sqlTransaction.primary_account,
+        secondaryAccount: sqlTransaction.secondary_account
+    } : null
+}
