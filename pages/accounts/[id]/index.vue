2b6e792812ebd0661f40b7fba3d036a47ed92faf
Sam Nguyen <sam.nguyen.1223@gmail.com>
2024-07-01 16:30:40 -0400

    Update frontend

--- pages/accounts/[id]/index.vue
+++ pages/accounts/[id]/index.vue
@@ -7,6 +7,7 @@ const accountMap = ref(new Map())
 let accountLoading = ref(true)
 const { data: accountData } = await useFetch(`/api/accounts/${route.params.id}`)
 const { data: transactionData, pending: transactionLoading } = await useFetch(`/api/accounts/${route.params.id}/transactions`)
+const { data: institutionData, pending: institutionLoading } = await useFetch(`/api/institutions/${accountData.value?.institutionId}`)
 
 $fetch('/api/accounts', {
     onResponse({ response }) {
@@ -19,6 +20,7 @@ $fetch('/api/accounts', {
     }
 })
 
+const institution: Institution = institutionData.value as Institution
 const account: Account = accountData.value as Account
 const transactions: Transaction[] = transactionData.value as unknown as Transaction[]
 
@@ -87,25 +89,33 @@ const createTransaction = (type?: TransactionTypes) => {
     </div>
     <div v-else>
         <div class="flex flex-wrap items-baseline mb-2">
-            <h1 class="text-5xl basis-auto">{{ account.name }} ...{{ account.accountNumber }}</h1>
-            <p class="justify-self-end text-2xl italic">
+            <h1 class="text-5xl basis-auto flex items-baseline gap-4">
+                <img :src="institution.icon" class="inline w-8 h-8"/>
+                <span>{{ account.name }} ...{{ account.accountNumber }}</span>
+            </h1>
+            <p class="sm:ml-auto text-2xl italic">
                 <span v-if="isLiability">$({{ balance }})</span>
                 <span v-else>${{ balance }}</span>
             </p>
             <FlexBreak />
             <p class="italic text-neutral-400">{{ account.accountType }}</p>
         </div>
-        <div class="flex justify-end gap-x-2 gap-y-1 flex-wrap">
-            <div class="dropdown justify-self-start xl:hidden">
-                <div tabindex="0" class="btn btn-primary text-lg" role="button">
+        <div class="flex gap-x-2 gap-y-1 flex-wrap w-full justify-normal">
+
+            <NuxtLink class="btn btn-primary text-lg font-normal" :to="`/accounts/${route.params.id}/edit`">Edit</NuxtLink>
+            <button class="btn btn-ghost w-fit text-lg font-light" @click="deleteAccount">Delete</button>
+            <FlexBreak class="sm:hidden" />
+
+            <div class="dropdown xl:hidden">
+                <div tabindex="0" class="btn btn-neutral text-lg font-normal" role="button">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                         stroke="currentColor" class="size-6">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
                     </svg>
                     New
                 </div>
-                <ul tabindex="0" class="menu dropdown-content shadow join join-vertical w-52 m-0">
-                    <button @click="createTransaction()" class="btn btn-sm btn-primary justify-start join-item">
+                <ul tabindex="0" class="menu dropdown-content shadow join join-vertical w-52 mt-2 p-0 z-[999]">
+                    <button @click="createTransaction()" class="btn btn-sm justify-start font-normal join-item">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                             stroke="currentColor" class="size-6">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
@@ -113,7 +123,7 @@ const createTransaction = (type?: TransactionTypes) => {
                         Transaction
                     </button>
                     <button v-for="transactionType in defaultTransactions" @click="createTransaction(transactionType)"
-                        class="btn btn-sm btn-primary join-item justify-start">
+                        class="btn btn-sm btn-neutral join-item justify-start">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                             stroke="currentColor" class="size-6">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
@@ -122,11 +132,10 @@ const createTransaction = (type?: TransactionTypes) => {
                     </button>
                 </ul>
             </div>
-            <NuxtLink class="btn btn-outline text-lg" :to="`/accounts/${route.params.id}/edit`">Edit</NuxtLink>
-            <button class="btn btn-outline btn-error w-fit text-lg" @click="deleteAccount">Delete</button>
+
             <FlexBreak />
             <div class="join hidden xl:block xl:join-horizontal">
-                <button @click="createTransaction()" class="btn btn-outline btn-sm btn-primary join-item">
+                <button @click="createTransaction()" class="btn btn-outline btn-sm btn-neutral font-normal join-item">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                         stroke="currentColor" class="size-6">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
@@ -134,7 +143,7 @@ const createTransaction = (type?: TransactionTypes) => {
                     Transaction
                 </button>
                 <button v-for="transactionType in defaultTransactions" @click="createTransaction(transactionType)"
-                    class="btn btn-outline btn-sm btn-primary join-item justify-start">
+                    class="btn btn-outline btn-sm btn-neutral join-item font-normal justify-start">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                         stroke="currentColor" class="size-6">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
@@ -143,16 +152,18 @@ const createTransaction = (type?: TransactionTypes) => {
                 </button>
             </div>
         </div>
+
         <div class="divider" />
-        <div class="flex flex-col bg-neutral shadow-xl py-2 rounded-lg" v-if="transactions.length > 0">
+
+        <div class="flex flex-col gap-1 bg-neutral shadow-xl py-6 rounded-lg" v-if="transactions.length > 0">
             <template v-for="transaction in transactions">
-                <TransactionListing class="px-4" :transaction="transaction"
+                <TransactionListing class="px-8" :transaction="transaction"
                     :primary-account="accountMap.get(transaction.primaryAccount)"
                     :secondary-account="accountMap.get(transaction.secondaryAccount)" />
                 <div class="divider my-0" />
             </template>
         </div>
-        <div v-else class="italic text-center">
+        <div v-else class="italic text-center text-neutral-400">
             No transactions found!
         </div>
     </div>
