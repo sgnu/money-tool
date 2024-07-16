1f9084673053ad917504ccd223c1921c76ecf255
Sam Nguyen <sam.nguyen.1223@gmail.com>
2024-07-16 16:53:55 -0400

    Split transaction creator to multi page action

--- pages/transactions/new.vue
+++ pages/transactions/new.vue
@@ -7,6 +7,8 @@ let categoryLoading = ref(true)
 const accounts = ref<Account[]>([])
 // const accountMap = ref(new Map<number, Account>())
 
+const step = ref(1)
+
 const categories = ref<Category[]>([])
 const categoryMap = ref(new Map<number, Category>())
 
@@ -35,13 +37,13 @@ await $fetch('/api/transactions/categories', {
 })
 
 const formData = reactive({
-    name: <string | null> null,
-    type: <TransactionTypes | null> null,
+    name: <string | null>null,
+    type: <TransactionTypes | null>null,
     date: initDate(),
     amount: null,
-    primaryAccount: <string | null> null,
-    secondaryAccount: <string | null> null,
-    category: <string | null> null
+    primaryAccount: <string | null>null,
+    secondaryAccount: <string | null>null,
+    category: <string | null>null
 })
 
 if (route.query.primaryAccount) {
@@ -72,7 +74,7 @@ const submit = () => {
         body: tempTransaction,
         onResponse() {
             setTimeout(() => {
-              navigateTo('/transactions')  
+                navigateTo('/transactions')
             })
         }
     })
@@ -84,7 +86,7 @@ function initDate() {
     // getMonth() is zero-indexed
     const month = `${date.getMonth() + 1 < 10 ? '0' + (date.getMonth() + 1) : date.getMonth() + 1}`
     const day = `${date.getDate() < 10 ? '0' + date.getDate() : date.getDate()}`
-    
+
     return `${date.getFullYear()}-${month}-${day}`
 }
 </script>
@@ -95,35 +97,73 @@ function initDate() {
             <FormCard v-if="accountLoading || categoryLoading">
                 <div class="skeleton w-full h-12"></div>
             </FormCard>
+
+            <FormCard v-else-if="true" @submit.prevent="submit">
+                <ul class="steps w-full mb-4">
+                    <li class="step cursor-pointer" :class="step > 0 ? 'step-primary' : ''" @click="step = 1">Info</li>
+                    <li class="step cursor-pointer" :class="step > 1 ? 'step-primary' : ''" @click="step = 2">Accounts
+                    </li>
+                    <li class="step cursor-pointer" :class="step > 2 ? 'step-primary' : ''" @click="step = 3">Category
+                    </li>
+                </ul>
+
+                <Transition name="page" mode="out-in">
+                    <div v-if="step === 1" class="flex flex-col gap-2 w-full">
+                        <label class="input input-bordered flex items-center gap-2 w-full">
+                            Name
+                            <input type="text" class="grow" v-model="formData.name" />
+                        </label>
+                        <select class="select select-bordered w-full" v-model="formData.type">
+                            <option disabled selected :value="null">Type</option>
+                            <option v-for="type in TransactionTypes">{{ type }}</option>
+                        </select>
+                        <label class="input input-bordered flex items-center gap-2 w-full">
+                            Amount
+                            <input type="text" class="grow" v-model="formData.amount" />
+                        </label>
+                        <input type="date" class="w-full px-4 py-2 bg-base-100 rounded" v-model="formData.date">
+                        <div class="flex gap-2 justify-end mt-2">
+                            <NuxtLink to="/transactions" class="btn btn-ghost" role="button">Cancel</NuxtLink>
+                            <div class="btn btn-primary" @click.prevent="step = 2" role="button">Next</div>
+                        </div>
+                    </div>
+
+                    <div v-else-if="step === 2" class="flex flex-col gap-2 w-full">
+                        <select class="select select-bordered w-full" v-model="formData.primaryAccount">
+                            <option disabled selected :value="null">Primary Account</option>
+                            <option v-for="account in (accounts)" :value="account.id">{{ account.name }} ...{{
+                account.accountNumber }}</option>
+                        </select>
+                        <select class="select select-bordered w-full" v-model="formData.secondaryAccount"
+                            :disabled="formData.type !== TransactionTypes.TRANSFER && formData.type !== TransactionTypes.PAYMENT">
+                            <option disabled selected :value="null">Secondary Account</option>
+                            <option v-for="account in (accounts)" :value="account.id">{{ account.name }} ..{{
+                account.accountNumber }}</option>
+                        </select>
+                        <div class="flex gap-2 justify-end mt-2">
+                            <div class="btn btn-ghost" @click.prevent="step = 1" role="button">Back</div>
+                            <div class="btn btn-primary" @click.prevent="step = 3" role="button">Next</div>
+                        </div>
+                    </div>
+
+                    <div v-else-if="step === 3" class="flex flex-col gap-2 w-full">
+                        <select class="select select-bordered w-full" v-model="formData.category">
+                            <option disabled selected>Category</option>
+                            <option :value="null" selected>Uncategorized</option>
+                            <option v-for="category in categories" :value="category.id"><span
+                                    v-if="category.icon && category.icon.length > 0">{{ category.icon }}</span> {{
+                category.name }}</option>
+                        </select>
+                        <div class="flex gap-2 justify-end mt-2">
+                            <div class="btn btn-ghost" @click.prevent="step = 2" role="button">Back</div>
+                            <input type="submit" class="btn btn-primary" value="Create" @submit="submit">
+                        </div>
+                    </div>
+                </Transition>
+            </FormCard>
+
             <FormCard v-else @submit.prevent="submit">
                 <h1 class="text-center text-2xl w-full pb-2">New Transaction</h1>
-                <label class="input input-bordered flex items-center gap-2 w-full">
-                    Name
-                    <input type="text" class="grow" v-model="formData.name" />
-                </label>
-                <select class="select select-bordered w-full" v-model="formData.type">
-                    <option disabled selected>Type</option>
-                    <option v-for="type in TransactionTypes">{{ type }}</option>
-                </select>
-                <select class="select select-bordered w-full" v-model="formData.category">
-                    <option disabled selected>Category</option>
-                    <option :value="null" selected>Uncategorized</option>
-                    <option v-for="category in categories" :value="category.id"><span v-if="category.icon && category.icon.length > 0">{{ category.icon }}</span> {{ category.name }}</option>
-                </select>
-                <input type="date" class="w-full px-4 py-2 bg-base-100 rounded" v-model="formData.date">
-                <label class="input input-bordered flex items-center gap-2 w-full">
-                    Amount
-                    <input type="text" class="grow" v-model="formData.amount" />
-                </label>
-                <select class="select select-bordered w-full" v-model="formData.primaryAccount">
-                    <option disabled selected>Primary Account</option>
-                    <option v-for="account in (accounts)" :value="account.id">{{ account.name }} ...{{ account.accountNumber }}</option>
-                </select>
-                <select class="select select-bordered w-full" v-model="formData.secondaryAccount">
-                    <option disabled selected>Secondary Account</option>
-                    <option v-for="account in (accounts)" :value="account.id">{{ account.name }} ..{{ account.accountNumber }}</option>
-                </select>
-                <input type="submit" class="btn btn-primary" @submit="submit">
             </FormCard>
         </Transition>
     </div>
@@ -134,6 +174,16 @@ function initDate() {
     TODO: style date picker to match daisyui
     maybe make a global style sheet
 */
+
+.page-enter-active,
+.page-leave-active {
+    transition: all 0.1s ease-out;
+}
+
+.page-enter-from,
+.page-leave-to {
+    opacity: 0;
+}
 
 .loading-form-enter-active,
 .loading-form-leave-active {
